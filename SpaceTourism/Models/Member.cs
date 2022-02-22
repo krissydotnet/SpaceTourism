c7fd7a294179b6b1b60c96ed7736fc61757d053c
Krissy Dukes <36676556+krissydotnet@users.noreply.github.com>
2022-02-22 15:12:49 -0500

    Work in progress crew page

--- SpaceTourism/Models/Member.cs
+++ SpaceTourism/Models/Member.cs
@@ -1,7 +1,7 @@
 ﻿namespace SpaceTourism.Models { 
-    public class CrewMembers
+    public class Member
     {
-
+        public int Index { get; set; }
         public string Name { get; set; }
 
         public string Title { get; set; }
@@ -11,5 +11,9 @@
         public string Image { get; set; }
         public string ImageAlt { get; set; }
 
+        public string ToTag()
+        {
+            return Title.Replace("", "-");
+        }
     }
 }
